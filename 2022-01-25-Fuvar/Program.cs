28f174eaad95148b7a74ae957e4c644ebf4bbaed
turbo1337 <torbam.12d@acsjszki.hu>
2022-01-25 12:56:30 +0100

    Add project files.

---
+++ 2022-01-25-Fuvar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2022_01_25_Fuvar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
