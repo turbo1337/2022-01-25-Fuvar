557f878438cca48502b24b56bed4d450714bc474
turbo1337 <torbam.12d@acsjszki.hu>
2022-01-25 13:16:38 +0100

    Fuvar 3

--- 2022-01-25-Fuvar/Program.cs
+++ 2022-01-25-Fuvar/Program.cs
@@ -11,8 +11,13 @@ namespace _2022_01_25_Fuvar
     {
         static void Main(string[] args)
         {
+            List<Fuvar> fuvarok = new List<Fuvar>();
+            foreach (var sor in File.ReadAllLines("Fuvar.csv").Skip(1))
+            {
+                fuvarok.Add(new Fuvar(sor));
+            }
 
-
+            Console.WriteLine($"3.feladat: {fuvarok.Count} fuvar");
 
             Console.ReadKey();
 
